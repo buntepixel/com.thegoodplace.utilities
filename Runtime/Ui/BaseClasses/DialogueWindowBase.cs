b1f885e590ef5b6e79bb1d5e0def9f92aae6b21f
Christian Freisleder <christian@buntepixel.eu>
2023-11-22 16:43:21 +0100

    add: infoPrefab fix: no propper default  inOutValue on fade;

--- Runtime/Ui/BaseClasses/DialogueWindowBase.cs
+++ Runtime/Ui/BaseClasses/DialogueWindowBase.cs
@@ -16,24 +16,24 @@ namespace TGP.Utilities.Ui {
 		public enum AnimationType {
 			fade,
 			moveRtL
-			}
+		}
 		[SerializeField]
 		protected RectTransform Mover;
 		[SerializeField]
 		AnimationType TypeOfAnimation;
 		[SerializeField]
 		[Tooltip("if animated In/Out")]
-		protected Vector2 In_Out_Value ;
-	
+		protected Vector2 In_Out_Value;
+		protected bool isOpen;
 		CanvasGroup cg;
 		protected virtual void Awake() {
 			cg = GetComponent<CanvasGroup>();
-			}
+		}
 		protected virtual void Start() {
 			cg.EnableInputVisibility(false);
 			switch (TypeOfAnimation) {
 				case AnimationType.fade:
-					In_Out_Value = new Vector2 (1, 0);
+					In_Out_Value = new Vector2(1, 0);
 					break;
 				case AnimationType.moveRtL:
 					Mover.anchoredPosition = new Vector2(In_Out_Value.y, Mover.anchoredPosition.y);
@@ -41,31 +41,31 @@ namespace TGP.Utilities.Ui {
 					break;
 				default:
 					break;
-				}
 			}
+		}
 
 
 		protected virtual void OpenWindow(System.Object sender, DialogueWindowBaseEventArgs args) {
 			if (debug)
 				Debug.LogFormat($"DiaogueWindowBase---OpenWindow: on:{this.gameObject.name}");
 
-
-			ButtonAnimation(In_Out_Value.x, args, TypeOfAnimation);
-		
-			
-			}
+			if (!isOpen)
+				ButtonAnimation(In_Out_Value.x, args, TypeOfAnimation);
+			isOpen = true;
+		}
 
 		protected virtual void CloseWindow(System.Object sender, DialogueWindowBaseEventArgs args) {
 			if (debug)
 				Debug.LogFormat($"DiaogueWindowBase---CloseWindow: on:{this.gameObject.name}");
 			ButtonAnimation(In_Out_Value.y, args, TypeOfAnimation);
-		
-			}
+			isOpen = false;
+
+		}
 
 		protected async void AutoHide(DialogueWindowBaseEventArgs args) {
 			await Task.Delay(args.DisplayDuration * 1000);
 			CloseWindow(this, args);
-			}
+		}
 		protected virtual void ButtonAnimation(float endValue, DialogueWindowBaseEventArgs args, AnimationType animType) {
 			switch (animType) {
 				case AnimationType.fade:
@@ -77,23 +77,23 @@ namespace TGP.Utilities.Ui {
 					if (Mover == null)
 						return;
 					Mover.DOAnchorPosX(endValue, args.TweenDuration).OnComplete(() => {
-						cg.EnableInputVisibility( In_Out_Value.x == endValue ? true : false);
+						cg.EnableInputVisibility(In_Out_Value.x == endValue ? true : false);
 					});
 					break;
 				default:
 					break;
-				}
-
 			}
 
 		}
+
+	}
 
 	public class DialogueWindowBaseEventArgs : EventArgs {
 		public DialogueWindowBaseEventArgs() {
 			TweenDuration = 0.3f;
-			}
-	
+		}
+
 		public float TweenDuration { get; set; }
 		public int DisplayDuration { get; set; }
-		}
 	}
+}
